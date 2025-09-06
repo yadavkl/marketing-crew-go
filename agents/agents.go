44a9e0dd01453eef4f895f2c8f3da7c05fff3268
Kanhaiya-Yadav <kanhaiya.yadav@ibm.com>
2025-09-06 11:55:33 +0530

    first commit

---
+++ agents/agents.go
@@ -0,0 +1,7 @@
+package agents
+
+import (
+	"github.com/nlpodyssey/openai-agents-go/agents"
+)
+
+var HeadMarketingAgent = agents.New("head_of_marketing")
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yadavkl/marketing-crew-go
+
+go 1.24.2
